af371b2de10a40f9dc3b1533288c24375b71c03e
Msamson09 <michaelssamson@gmail.com>
2021-07-27 16:38:17 -0700

    adds crud

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,12 +3,36 @@ import { Post } from '../models/post.js'
 export {
     index,
     create,
-    show,
+    postDelete as delete,
+    editPost as edit,
+    update
 }
 
-function show(req, res) {
-
+function update(req, res) {
+    Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(post => {
+        res.redirect('/posts')
+    })
 }
+
+function editPost(req, res) {
+    Post.findById(req.params.id)
+    .populate('author')
+    .then(post => {
+        res.render('posts/edit', {
+            post
+        })
+
+    })    
+}
+
+function postDelete(req, res) {
+    Post.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.redirect('/posts')
+    })
+}
+
 
 function create(req, res) {
     req.body.author = req.user.profile._id
--- routes/posts.js
+++ routes/posts.js
@@ -9,8 +9,9 @@ const router = Router()
 
 router.get('/', isLoggedIn, postsCtrl.index)
 router.post('/', isLoggedIn, postsCtrl.create)
-router.get('/:id', isLoggedIn, postsCtrl.show)
-// router.post('/:id', isLoggedIn, posts.reply)
+router.delete('/:id', isLoggedIn, postsCtrl.delete)
+router.put('/:id', isLoggedIn, postsCtrl.update)
+router.get('/:id/edit', isLoggedIn, postsCtrl.edit)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
